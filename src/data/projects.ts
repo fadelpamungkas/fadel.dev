3b2195e80e42bac232c4ea6c7746df98714e75c1
Fadel Pamungkas <fadelpamungkas3@gmail.com>
2023-08-04 13:49:11 +0700

    fix(project): change projects description

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -9,7 +9,7 @@ interface Project {
 const projects: Project[] = [
   {
     title: "Receipt",
-    description: "Receipts generator that can be used for invoicing, order confirmation, or any other transaction-related documents.",
+    description: "Receipts generator that can be used for any transaction-related documents.",
     techs: ["Go"],
     link: "https://github.com/fadelpamungkas/receipt",
   },
@@ -21,31 +21,33 @@ const projects: Project[] = [
   },
   {
     title: "Fulltext Job Search Engine",
-    description: "Implementing fulltext search using meilisearch and Go",
+    description: "Fulltext search engine for job search using MeiliSearch",
     techs: ["Go"],
     link: "https://github.com/fadelpamungkas/fulltext-search.git",
   },
   {
+// Zero To Production is the ideal starting point for your journey as a Rust backend developer.
+// You will learn by doing: you will build a fully functional email newsletter API, starting from scratch.
     title: "Newsletter Service",
-    description: "A simple newsletter service using Rust",
+    description: "Zero To Production book's implementation to build a fully functional email newsletter API",
     techs: ["Rust"],
     link: "https://github.com/fadelpamungkas/newsletter-service",
   },
   {
     title: "Chip4 Editor",
-    description: "A simple chip8 emulator using Rust",
+    description: "Minimalist vim-like command line text editor",
     techs: ["Rust"],
     link: "https://github.com/fadelpamungkas/chip4editor",
   },
   {
-    title: "Snapwork",
-    description: "A simple job portal using Go",
+    title: "SnapWork",
+    description: "Job portal that helps job seekers to find their dream job and help companies to find the best talent.",
     techs: ["Go"],
     link: "https://github.com/fadelpamungkas/snapwork-api",
   },
   {
     title: "Wallet Engine",
-    description: "A simple wallet engine using Go",
+    description: "Wallet service that can be used for e-wallet, payment gateway, or any other transaction-related services.",
     techs: ["Go"],
     link: "https://github.com/fadelpamungkas/engine",
   },
